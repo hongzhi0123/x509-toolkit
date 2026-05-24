10e3f3da25fd50492b13654730a841f394ecdefb
hongzhi0123 <55428217+hongzhi0123@users.noreply.github.com>
2026-05-24 11:07:05 +0200

    Open certificate file via context menu

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,14 @@ import * as vscode from 'vscode';
 import { openCreateCertPanel } from './panels/createCertPanel';
 import { showFromSelection } from './commands/showFromSelection';
 import { openFile } from './commands/openFile';
+import { openFromExplorer } from './commands/openFromExplorer';
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand('x509toolkit.showFromSelection', showFromSelection(context)),
     vscode.commands.registerCommand('x509toolkit.openFile', openFile(context)),
-    vscode.commands.registerCommand('x509toolkit.createCertificate', openCreateCertPanel(context))
+    vscode.commands.registerCommand('x509toolkit.createCertificate', openCreateCertPanel(context)),
+    vscode.commands.registerCommand('x509toolkit.openFromExplorer', openFromExplorer(context))
   );
 }
 
